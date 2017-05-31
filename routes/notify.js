dfaf146597951e80db6f0ef32a3047208d082248
HanQi <HanQi@hanqideMBP.lan>
2017-05-31 14:12:04 +0800

    pay

---
+++ routes/notify.js
@@ -0,0 +1,15 @@
+/**
+ * Created by HanQi on 2017/5/31.
+ */
+
+router.post('/', function(req, res, next) {
+
+    console.log('支付成功post');
+
+});
+
+router.get('/', function(req, res, next) {
+
+    console.log('支付成功get');
+
+});
--- routes/user.js
+++ routes/user.js
@@ -106,6 +106,199 @@ router.post('/saveUserInfo', function(req, res, next) {
 
 });
 
+router.post('/login', function(req, res, next) {
 
+    var phone = req.body.phone;
+    var psw = req.body.psw;
+
+    var query = new AV.Query(User);
+    query.equalTo('phone', phone);
+    query.find().then(function (results) {
+
+        if (results.length == 0) {
+
+            var wrap = {
+
+                'code' : 101,
+                'data' : '',
+                'message' : '该用户未注册'
+
+            };
+
+            res.send(wrap);
+
+        } else {
+
+            var user = results[0];
+
+            if (user.get('psw') == psw) {
+
+                var data = {
+
+                    'uid':user.get('objectId'),
+                    'cover':user.get('imageUrl') == null ? '' : user.get('imageUrl'),
+                    'money':user.get('accountMoney') == null ? '0' : user.get('accountMoney'),
+                    'nickname':user.get('nickname') == null ? '' : user.get('nickname'),
+                    'realname':user.get('username') == null ? '' : user.get('username'),
+                    'sex':user.get('sex') == null ? '0' : user.get('sex'),
+                    'age':user.get('age') == null ? '0' : user.get('age'),
+                    'point':user.get('point') == null ? '0' : user.get('point')
+
+                }
+
+                var wrap = {
+
+                    'code' : 200,
+                    'data' : data,
+                    'message' : '登录成功'
+
+                };
+
+                res.send(wrap);
+
+            } else {
+
+                var wrap = {
+
+                    'code' : 102,
+                    'data' : '',
+                    'message' : '用户名或密码错误'
+
+                };
+
+                res.send(wrap);
+
+            }
+
+        }
+
+
+
+
+
+
+    });
+
+
+});
+
+router.post('/regist', function(req, res, next) {
+
+    var phone = req.body.phone;
+    var psw = req.body.psw;
+
+    var query = new AV.Query(User);
+    query.equalTo('phone', phone);
+    query.find().then(function (results) {
+
+        if (results.length == 0) {
+
+            var user = new AV.User();
+
+            user.setUsername(phone);
+            user.setPassword(psw);
+
+            user.set('phone', phone);
+            user.set('psw', psw);
+
+            console.log(user);
+
+            user.save().then(function (user) {
+
+                var data = {
+
+                    'uid':user.get('objectId')
+
+                }
+
+                var wrap = {
+
+                    'code' : 200,
+                    'data' : data,
+                    'message' : ''
+
+                };
+
+                res.send(wrap);
+
+            });
+
+        } else {
+
+            var wrap = {
+
+                'code' : 101,
+                'data' : '',
+                'message' : '该用户已注册'
+
+            };
+
+            res.send(wrap);
+
+
+
+        }
+
+
+
+
+
+
+    });
+
+
+
+});
+
+router.post('/findpsw', function(req, res, next) {
+
+    var phone = req.body.phone;
+    var psw = req.body.psw;
+
+    var query = new AV.Query(User);
+    query.equalTo('phone', phone);
+    query.find().then(function (results) {
+
+        if (results.length == 0) {
+
+            var wrap = {
+
+                'code' : 101,
+                'data' : '',
+                'message' : '该用户未注册'
+
+            };
+
+            res.send(wrap);
+
+        } else {
+
+            var user = results[0];
+
+            user.set('psw', psw);
+            user.save().then(function (user) {
+
+                var wrap = {
+
+                    'code' : 200,
+                    'data' : '',
+                    'message' : '操作成功'
+
+                };
+
+                res.send(wrap);
+
+            })
+
+        }
+
+
+
+
+
+
+    });
+
+});
 
 module.exports = router;
